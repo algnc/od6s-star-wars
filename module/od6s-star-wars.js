55f46138090a1bc83e54eb08947361855bdb8187
DarthBanjo <99984933+DarthBanjo@users.noreply.github.com>
2023-06-30 16:31:17 -0400

    add google font links

--- module/od6s-star-wars.js
+++ module/od6s-star-wars.js
@@ -11,3 +11,21 @@ Hooks.once('init', async function () {
   // Register sheet application classes
   Actors.registerSheet('od6s-star-wars',StarWarsSheets.ActorSheetStarWars, {label: "od6s-star-wars.ActorSheetStarWars"})
 })
+
+
+//fontloader: Pull Google Fonts when Foundry loads instead of each time character sheet opens.
+var preconnectLink1 = document.createElement('link');
+preconnectLink1.rel = 'preconnect';
+preconnectLink1.href = 'https://fonts.googleapis.com';
+document.head.appendChild(preconnectLink1);
+
+var preconnectLink2 = document.createElement('link');
+preconnectLink2.rel = 'preconnect';
+preconnectLink2.href = 'https://fonts.gstatic.com';
+preconnectLink2.crossOrigin = true; // Add crossorigin attribute for cross-origin request
+document.head.appendChild(preconnectLink2);
+
+var fontStylesheet = document.createElement('link');
+fontStylesheet.href = 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;500;600;700&family=DM+Serif+Display&family=Domine:wght@400;700&family=Varela&display=swap';
+fontStylesheet.rel = 'stylesheet';
+document.head.appendChild(fontStylesheet);
